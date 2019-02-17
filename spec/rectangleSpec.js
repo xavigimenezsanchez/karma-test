ba6b5367459ff6606236f3417c0906b76d0fa684
Xavi <javiergimenezsanchez@gmail.com>
2019-02-17 21:17:24 +0100

    First commit

---
+++ spec/rectangleSpec.js
@@ -0,0 +1,16 @@
+describe("Class Rectangle", function() {
+    var rectangle,
+        height = 2,
+        width = 3;
+  
+    beforeEach(() => {
+        rectangle = new Rectangle(height, width)
+    });
+    it("Area calculate", function() {  
+      expect(rectangle.calcArea()).toBe(height * width);
+    });
+
+    it("get is working correctly", () => {
+        expect(rectangle.area).toBe(height * width);
+    })
+  });
